19b9112bf86d00d8451fc3c55f24125ac1a07e49
diksha1627 <66207475+diksha1627@users.noreply.github.com>
2022-06-11 16:53:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React,{Component} from 'react';
+import './App.css';
+import Clock from './Clock';
+
+
+
+class App extends Component {
+
+
+  state = {
+
+    secondRatio: 0,
+    minuteRatio: 0,
+    hourRatio: 0
+  }
+
+  componentDidMount () {
+    setInterval(()=> {
+      this.setClock()
+    },1000)
+  }
+
+  setClock = () =>{
+    const currentDate = new Date;
+    let secondRatio = currentDate.getSeconds()/60;
+    let minuteRatio = (secondRatio + currentDate.getMinutes())/60;
+    let hourRatio = (minuteRatio + currentDate.getHours()) /12;
+
+    this.setState({secondRatio: secondRatio})
+    this.setState({minuteRatio: minuteRatio})
+    this.setState({hourRatio: hourRatio})
+  }
+
+  render()
+  {
+    const {secondRatio, minuteRatio, hourRatio} = this.state
+    return (
+      <>
+    
+    <Clock secondRatio={secondRatio} minuteRatio={minuteRatio} 
+            hourRatio={hourRatio}/>
+    </>
+    );
+
+  }
+ 
+}
+
+export default App;
